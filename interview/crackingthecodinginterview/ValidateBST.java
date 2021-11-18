34802d1942ee19d75e9367c685367d5654a31766
shanurrahman-rapido <shanur.rahman@rapido.bike>
2021-11-18 10:45:37 +0530

    Check if Binary tree is a valid BST

---
+++ interview/crackingthecodinginterview/ValidateBST.java
@@ -0,0 +1,30 @@
+package interview.crackingthecodinginterview;
+
+class Tree {
+    int data;
+    public Tree left, right;
+
+    Tree(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+
+public class ValidateBST {
+    public static void main(String[] args) {
+        Tree root = new Tree(100);
+        System.out.println(isBST(root, Integer.MAX_VALUE, Integer.MIN_VALUE));
+    }
+    
+
+    public static boolean isBST(Tree root, int min, int max) {
+        if (root == null) {
+            return true;
+        }
+
+        if (root.data > max || root.data < min) {
+            return false;
+        }
+        return isBST(root.left, min, root.data) && isBST(root.right, root.data, max);
+    }
+}
