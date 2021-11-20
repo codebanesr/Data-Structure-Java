97fa95a9e66a2ecfa1052a29a46ce09d804b0544
shanurrahman-rapido <shanur.rahman@rapido.bike>
2021-11-20 16:04:56 +0530

    renamed to powerset

---
+++ interview/sprint2/PowerSet.java
@@ -0,0 +1,26 @@
+package interview.sprint2;
+
+import java.util.LinkedList;
+
+public class PowerSet {
+    static LinkedList<String> powerSet = new LinkedList<>();
+    public static void main(String[] args) {
+        powerSet("abc", "", 0, "abc".length());
+        for (String s : powerSet) {
+            System.out.println(s);
+        }
+    }
+    
+
+    // The relative ordering will not change in this case, this is not generating all the permutations of the
+    // given string, only the subsets
+    public static void powerSet(String word, String s, int index, int total) {
+        if (index >= total) {
+            powerSet.add(s);
+            return;
+        }
+
+        powerSet(word, s+word.charAt(index), index+1, total);
+        powerSet(word, s, index+1, total);
+    }
+}
