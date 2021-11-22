c56130f3d86276bdf60af5fd7698e64f620d59df
Shanur <shanur.cse.nitap@gmail.com>
2021-11-22 11:10:47 +0530

    added implementation for printing all permutation with duplicate characters

---
+++ interview/crackingthecodinginterview/Dynamic/TowerOfHanoi.java
@@ -0,0 +1,9 @@
+package interview.crackingthecodinginterview.Dynamic;
+
+public class TowerOfHanoi {
+    public static void main(String args[]) {
+        moveDisk(3, "tower1", "tower2", "tower3");
+    }
+
+    public static moveDisk(int n, int source, int )
+}
