3c00264a84035ed28695b07a19542c86a31ad73d
shanurrahman-rapido <shanur.rahman@rapido.bike>
2021-11-26 00:36:12 +0530

    Activity selection problem implemented

---
+++ practicesection/ActivitySelection.java
@@ -0,0 +1,45 @@
+package practicesection;
+
+import java.util.*;
+
+class Activity {
+    int startTime, endTime;
+
+    Activity(int startTime, int endTime) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public String toString() {
+        return "start -> " + this.startTime + ", " + "end -->" + this.endTime;
+    }
+}
+
+// sort based on end time, push first element since it is not dependent on
+// any other element. From next element onwards you need to check if
+// previous job has been finished
+public class ActivitySelection {
+    public static void main(String[] args) {
+        int s[] = { 1, 3, 0, 5, 8, 5 };
+        int f[] = { 2, 4, 6, 7, 9, 9 };
+
+        Activity activities[] = new Activity[s.length];
+        for (int i = 0; i < s.length; i++) {
+            activities[i] = new Activity(s[i], f[i]);
+        }
+
+        Arrays.sort(activities, (a1, a2) -> Integer.compare(a1.endTime, a2.endTime));
+        Stack<Activity> stack = new Stack<>();
+        stack.push(activities[0]);
+        for (int i = 1; i < activities.length; i++) {
+            if (activities[i].startTime >= stack.peek().endTime) {
+                stack.push(activities[i]);
+            }
+        }
+
+
+        for (Activity a : stack) {
+            System.out.println(a);
+        }
+    }
+}
