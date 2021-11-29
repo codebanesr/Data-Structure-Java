6b73a5ebcdbbffdff95517c53e6ce2d9e337c30a
shanurrahman-rapido <shanur.rahman@rapido.bike>
2021-11-29 15:55:49 +0530

    Longest string without repeating characters

---
+++ practicesection/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,48 @@
+package practicesection;
+
+import java.util.*;
+
+class ResultPair {
+    int start, end;
+}
+
+class LongestSubstringWithoutRepeatingCharacters {
+    public static void main(String args[]) {
+        String s = "ylllllasdfghj3";
+        char result[] = solve(s.toCharArray());
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static char[] solve(char charr[]) {
+        int length = 0, maxLength = Integer.MIN_VALUE;
+        ResultPair rp = new ResultPair();
+        HashSet<Character> hset = new HashSet<>();
+        int start = 0, end = 0;
+        for (int i = 0; i < charr.length; i++) {
+            if (!hset.contains(charr[i])) {
+                end++;
+            } else {
+                hset.clear();
+                length = end - start;
+                if (length > maxLength) {
+                    maxLength = length;
+                    rp.start = start;
+                    rp.end = end - 1;
+                }
+                start = i;
+                end = i;
+            }
+
+            hset.add(charr[i]);
+        }
+
+        // if the longest string comes at last, we will not arrive at the else condition
+        // because the for loop itself terminates ..., therefore we need this check at the end
+        if (end - start > rp.end - rp.start) {
+            rp.end = end;
+            rp.start = start;
+        }
+
+        return Arrays.copyOfRange(charr, rp.start, rp.end + 1);
+    }
+}
--- practicesection/REMAINING.md
+++ practicesection/REMAINING.md
@@ -4,7 +4,11 @@ KRUSKALS
 FLOYD WARSHALL
 KHIGHTS PROBLEM BACKTRACKING [ using bfs ]
 
-
 # Frequently asked problems
 EXTERNAL SORT
 DATA STRUCTURE WITH O(1) EVERYTHIHNG
+
+
+# Data structure java
+LinkedHashMap
+PrioiryQueue implementation
