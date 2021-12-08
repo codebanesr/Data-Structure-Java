6c4d8df437b0f49b1b82bc9ab52790d0941539e2
shanurrahman-rapido <shanur.rahman@rapido.bike>
2021-12-09 01:25:37 +0530

    Adding commits to infix to postfix expressions

--- practicesection/InfixToPostfix.java
+++ practicesection/InfixToPostfix.java
@@ -1,5 +1,3 @@
-package practicesection;
-
 import java.util.*;
 
 class InfixtoPostfix {
@@ -18,19 +16,24 @@ class InfixtoPostfix {
         precedenceMap.put('^', 5);
     }
 
+
+    // in postfix operators come after the operand, but operators with higher
+    // precedence come first ....
+    // If you see an operand add it to output
+    // if you see operand check the stack - If empty push the operator
+    // if higher precedence is present on stack it add stack top to output and then push this
+    // current one to top of stack... handle the brackets separately ....
     static String generatePostfix(char charr[]) {
         onInit();
         String postfix = "";
         Stack<Character> st = new Stack<>();
         for (char c : charr) {
-
             if (Character.isLetterOrDigit(c)) {
                 postfix += c;
-            }
-            else if (c == '(') {
+            } else if (c == '(') {
                 st.push('(');
             }
-            
+
             else if (c == ')') {
                 while (!st.isEmpty() && st.peek() != '(')
                     postfix += st.pop();
@@ -39,7 +42,7 @@ class InfixtoPostfix {
 
             else {
                 // if(!st.isEmpty())
-                //     System.out.println(st.peek() + " " + c);
+                // System.out.println(st.peek() + " " + c);
                 while (!st.isEmpty() && precedenceMap.getOrDefault(st.peek(), -1) > precedenceMap.getOrDefault(c, -1)) {
                     postfix += st.pop();
                 }
